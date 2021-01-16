1ba5449988bd2970191272cf2d831a3aea30acdc
Tashrif Sanil <7899547+tashrifsanil@users.noreply.github.com>
2021-01-16 02:28:13 -0500

    Added working page switching functionality

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ import AddIcon from "@material-ui/icons/Add";
 import EditIcon from "@material-ui/icons/Edit";
 import UpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { green, red } from "@material-ui/core/colors";
-import clsx from "clsx";
+import ChevronRightOutlinedIcon from "@material-ui/icons/ChevronRightOutlined";
+import ChevronLeftOutlinedIcon from "@material-ui/icons/ChevronLeftOutlined";
 
-const useStyles = makeStyles((theme) => ({
+const pageControlStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.background.paper,
     width: 600,
@@ -33,28 +34,91 @@ const useStyles = makeStyles((theme) => ({
   prevPageFab: {
     position: "absolute",
     color: theme.palette.common.white,
-    backgroundColor: green[500],
-    "&:hover": {
-      backgroundColor: green[600],
-    },
+    // backgroundColor: green[500],
+    // "&:hover": {
+    //   backgroundColor: green[600],
+    // },
     bottom: theme.spacing(5),
     right: theme.spacing(25),
   },
 }));
 
+// Page control floating buttons
+const PageControlFABS = (props) => {
+  const classes = pageControlStyles();
+  const theme = useTheme();
+  // currentPage is an integer indicating current page number
+  var fabs = [
+    {
+      color: "inherit",
+      className: classes.nextPageFab,
+      icon: <ChevronRightOutlinedIcon />,
+      label: "Next",
+      clickHandler: (event) => {
+        var newPageCount = props.currentPage + 1;
+        if (newPageCount <= props.maxPages) {
+          props.setCurrentPage(newPageCount);
+          console.log("Current page changed ", props.currentPage);
+        }
+      },
+    },
+    {
+      color: "secondary",
+      className: classes.pageIndicatorFab,
+      icon: `${props.currentPage}/100`,
+      label: `${props.currentPage}/100`,
+      clickHandler: (event) => {},
+    },
+    {
+      color: "inherit",
+      className: classes.prevPageFab,
+      icon: <ChevronLeftOutlinedIcon />,
+      label: "Prev",
+      clickHandler: (event) => {
+        var newPageCount = props.currentPage - 1;
+        if (newPageCount >= props.minPages) {
+          props.setCurrentPage(newPageCount);
+          console.log("prev current page changed ", newPageCount);
+        }
+      },
+    },
+  ];
+
+  return (
+    <>
+      {fabs.map((fab, index) => (
+        <Fab
+          aria-label={fab.label}
+          className={fab.className}
+          color={fab.color}
+          onClick={fab.clickHandler}
+        >
+          {fab.icon}
+        </Fab>
+      ))}
+    </>
+  );
+};
+
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   // This gets run everytime the state of searchTerm changes
   useEffect(() => {
     console.log("Search term was changed, ", searchTerm);
     searchMovieRequest();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchTerm]);
+  }, [searchTerm, currentPage]);
 
   const searchMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=" + searchTerm + "&apikey=a7d62505";
+    const url =
+      "http://www.omdbapi.com/?s=" +
+      searchTerm +
+      "&apikey=a7d62505" +
+      "&page=" +
+      currentPage;
     const response = await fetch(url);
 
     const responseJson = await response.json();
@@ -65,36 +129,6 @@ function App() {
     }
   };
 
-  const classes = useStyles();
-  const theme = useTheme();
-
-  var fabs = [
-    {
-      color: "primary",
-      className: classes.nextPageFab,
-      icon: <AddIcon />,
-      label: "Add",
-    },
-    {
-      color: "secondary",
-      className: classes.pageIndicatorFab,
-      icon: "1/100",
-      label: "Edit",
-    },
-    {
-      color: "inherit",
-      className: classes.prevPageFab,
-      icon: <UpIcon />,
-      label: "Expand",
-    },
-  ];
-
-  // const fab = fabs[0];
-  const test = {
-    className: classes.testButton,
-  };
-  const index = 0;
-
   return (
     // Use bootstrap styles for root container
     <>
@@ -103,15 +137,12 @@ function App() {
         <div className="row">
           <MovieGrid movies={movies}></MovieGrid>
         </div>
-        {fabs.map((fab, index) => (
-          <Fab
-            aria-label={fab.label}
-            className={fab.className}
-            color={fab.color}
-          >
-            {fab.icon}
-          </Fab>
-        ))}
+        <PageControlFABS
+          maxPages={100}
+          minPages={1}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        ></PageControlFABS>
       </div>
     </>
   );
