b10337c7198e02d30e1eb60b140e1e8b8f7016f9
Tashrif Sanil <7899547+tashrifsanil@users.noreply.github.com>
2021-01-16 01:04:07 -0500

    Added fab (floating above button), later to be used for page switching

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import SearchBar from './components/SearchBar';
-import MovieGrid from './components/MovieGrid';
-import NavBar from './components/NavBar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import SearchBar from "./components/SearchBar";
+import MovieGrid from "./components/MovieGrid";
+import NavBar from "./components/NavBar";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AddIcon from "@material-ui/icons/Add";
+import EditIcon from "@material-ui/icons/Edit";
+import UpIcon from "@material-ui/icons/KeyboardArrowUp";
+import { green, red } from "@material-ui/core/colors";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 600,
+    position: "relative",
+    minHeight: 200,
+  },
+  nextPageFab: {
+    position: "absolute",
+    bottom: theme.spacing(5),
+    right: theme.spacing(5),
+  },
+  pageIndicatorFab: {
+    position: "absolute",
+    bottom: theme.spacing(5),
+    right: theme.spacing(15),
+  },
+  prevPageFab: {
+    position: "absolute",
+    color: theme.palette.common.white,
+    backgroundColor: green[500],
+    "&:hover": {
+      backgroundColor: green[600],
+    },
+    bottom: theme.spacing(5),
+    right: theme.spacing(25),
+  },
+}));
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
   // This gets run everytime the state of searchTerm changes
   useEffect(() => {
     console.log("Search term was changed, ", searchTerm);
     searchMovieRequest();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchTerm])
+  }, [searchTerm]);
 
   const searchMovieRequest = async () => {
     const url = "http://www.omdbapi.com/?s=" + searchTerm + "&apikey=a7d62505";
@@ -24,19 +61,59 @@ function App() {
 
     // If search results returned are not empty
     if (responseJson.Search) {
-      setMovies(responseJson.Search)
+      setMovies(responseJson.Search);
     }
   };
 
+  const classes = useStyles();
+  const theme = useTheme();
+
+  var fabs = [
+    {
+      color: "primary",
+      className: classes.nextPageFab,
+      icon: <AddIcon />,
+      label: "Add",
+    },
+    {
+      color: "secondary",
+      className: classes.pageIndicatorFab,
+      icon: "1/100",
+      label: "Edit",
+    },
+    {
+      color: "inherit",
+      className: classes.prevPageFab,
+      icon: <UpIcon />,
+      label: "Expand",
+    },
+  ];
+
+  // const fab = fabs[0];
+  const test = {
+    className: classes.testButton,
+  };
+  const index = 0;
 
   return (
     // Use bootstrap styles for root container
-    <div className="container-fluid">
+    <>
       <NavBar setSearchTerm={setSearchTerm} />
-      <div className="row">
-        <MovieGrid movies={movies}></MovieGrid>
+      <div className="container-fluid">
+        <div className="row">
+          <MovieGrid movies={movies}></MovieGrid>
+        </div>
+        {fabs.map((fab, index) => (
+          <Fab
+            aria-label={fab.label}
+            className={fab.className}
+            color={fab.color}
+          >
+            {fab.icon}
+          </Fab>
+        ))}
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,96 +8,96 @@ import SearchIcon from "@material-ui/icons/Search";
 import MovieIcon from "@material-ui/icons/Movie";
 
 const useStyles = makeStyles((theme) => ({
-    appBar: {
-        backgroundColor: "#181818",
+  appBar: {
+    backgroundColor: "#181818",
+  },
+  root: {
+    flexGrow: 1,
+  },
+  movieIcon: {
+    width: "3%",
+    height: "3%",
+    marginRight: theme.spacing(2),
+    color: "white", //"#ff316f",
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily: "Open Sans Light",
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
     },
-    root: {
-        flexGrow: 1,
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    movieIcon: {
-        width: "2%",
-        height: "2%",
-        marginRight: theme.spacing(2),
-        color: "white", //"#ff316f",
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "30%",
     },
-    title: {
-        flexGrow: 1,
-        fontFamily: "Open Sans Light",
-        display: "none",
-        [theme.breakpoints.up("sm")]: {
-            display: "block"
-        }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
     },
-    search: {
-        position: "relative",
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        "&:hover": {
-            backgroundColor: fade(theme.palette.common.white, 0.25)
-        },
-        marginLeft: 0,
-        width: "100%",
-        [theme.breakpoints.up("sm")]: {
-            marginLeft: theme.spacing(1),
-            width: "30%"
-        }
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: "100%",
-        position: "absolute",
-        pointerEvents: "none",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center"
-    },
-    inputRoot: {
-        color: "inherit"
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create("width"),
-        width: "100%",
-        [theme.breakpoints.up("sm")]: {
-            width: "12ch",
-            "&:focus": {
-                width: "20ch"
-            }
-        }
-    }
+  },
 }));
 
 const NavBar = (props) => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static" className={classes.appBar}>
-                <Toolbar>
-                    <MovieIcon className={classes.movieIcon} />
-                    <Typography className={classes.title} variant="h4" noWrap>
-                        Movies
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <MovieIcon className={classes.movieIcon} />
+          <Typography className={classes.title} variant="h4" noWrap>
+            Movies
           </Typography>
-                    <div className={classes.search}>
-                        <div className={classes.searchIcon}>
-                            <SearchIcon />
-                        </div>
-                        <InputBase
-                            placeholder="Search…"
-                            classes={{
-                                root: classes.inputRoot,
-                                input: classes.inputInput
-                            }}
-                            inputProps={{ "aria-label": "search" }}
-                            onChange={(event) => props.setSearchTerm(event.target.value)}
-                        />
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
-}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+              onChange={(event) => props.setSearchTerm(event.target.value)}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export default NavBar;
