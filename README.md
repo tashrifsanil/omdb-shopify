8c1583f637ae6105100380f3aa9184a9e6a014f6
Tashrif Sanil <tashrif@localhost.localdomain>
2021-05-03 13:15:14 -0400

    Purged legacy files, starting over

--- README.md
+++ README.md
@@ -1,42 +1,43 @@
-# Shopify front end internship challenge
+# Example App: Create React App
 
-To use this app, just start searching for a movie title and search results (search term has to be atleast 3 characters) will appear on the left pane. Clicking on the red page indicator will bring up a page switcher dialog. Click the plus button on the search results to nominate, and the nominations will appear on the right pane. When you reach 5 nominations, a dialog will automatically pop up and show the movies you nominated
+Demonstrates Polaris inside a [create-react-app](https://github.com/facebookincubator/create-react-app) project with minimal setup and configuration.
 
-Clicking on a search result will spawn up a layout which will give you more details on that specific movie
+## Dependencies
 
-# Access
+- [yarn](https://github.com/yarnpkg/yarn/) or [npm](https://www.npmjs.com/)
 
-The app is hosted at https://tashrifsanil.github.io/omdb-shopify/
+## Getting started
 
-## Building the project
+### Installation
 
-After cloing the repo, in the project directory run the following to download any dependencies
+Install all the project dependencies.
 
-### `yarn`
+**With Yarn**
 
-Then to run the project just run the following in the project directory.
+```bash
+yarn install
+```
 
-### `yarn start`
+**With npm**
 
-This runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+```bash
+npm install
+```
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+### Development
 
-### `yarn test`
+Run the local `create-react-app` development server with Polaris preconfigured.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+**With Yarn**
 
-### `yarn build`
+```bash
+yarn start
+```
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+**With npm**
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+```bash
+npm start
+```
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
+Open **http://localhost:3000** in your browser and you should see the Polaris example application.
--- src/App.js
+++ src/App.js
@@ -1,183 +1,28 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import "fontsource-roboto";
-import "@fontsource/open-sans";
+import React, { useState, useCallback } from 'react';
+import {
+  Layout,
+  Page,
+  FooterHelp,
+  Card,
+  Link,
+  Button,
+  FormLayout,
+  TextField,
+  AccountConnection,
+  ChoiceList,
+  SettingToggle,
+} from '@shopify/polaris';
+import { ImportMinor } from '@shopify/polaris-icons';
 
-import React, { useEffect, useState } from "react";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
+export default function App() {
 
-import AlertDialog from "./components/AlertDialog";
-// import DialogContentText from '@material-ui/core/DialogContentText';
-import Grid from "@material-ui/core/Grid";
-import MovieMoreInfo from "./components/MovieMoreInfo";
-import NominatedMovies from "./components/NominatedMovies";
-import PageControl from "./components/PageControl";
-import SearchAppBar from "./components/SearchAppBar";
-import SearchResults from "./components/SearchResults";
-import Typography from "@material-ui/core/Typography";
-import { Helmet } from 'react-helmet';
-
-const useStyles = makeStyles((theme) => ({
-  nominationsHeader: {
-    paddingLeft: "1%",
-    fontFamily: "Open Sans Light",
-    width: "100%",
-  },
-}));
-
-function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResultsVisible, setSearchResultsVisibility] = useState(false);
-  const [nominationsVisible, setNominationsVisibility] = useState(false);
-
-  const [currentPage, setCurrentPage] = useState(1);
-  const [nominatedMoviesList, setNominatedMoviesList] = useState([]);
-  const [maxPages, setMaxPages] = useState(1);
-
-  const [nominationsCompleted, setNomCompleted] = useState(false);
-  const maxNominations = 5;
-
-  const [showMovieMoreInfo, setShowMovieMoreInfo] = useState(false);
-
-  // The movie to display in more info section
-  const [movie4MoreInfo, setMovie4MoreInfo] = useState({});
-
-  const onSearchEntryClicked = (movie) => {
-    setMovie4MoreInfo(movie);
-    console.log("Search Entry clicked ", movie4MoreInfo);
-    setShowMovieMoreInfo(true);
-  };
-
-  useEffect(() => {
-    const nominatedMovies = JSON.parse(
-      localStorage.getItem("omdb-app-nominations")
-    );
-    console.log("Nominated movies local storage ", nominatedMovies);
-    // If we have nothing in local storage our array can be none
-    // so to avoid that we check if its before setting the state of nominated nominated movies
-    // from local storage
-    if (nominatedMovies) {
-      setNominatedMoviesList(nominatedMovies);
-    }
-  }, []);
-
-  useEffect(() => {
-    // Search term has changed, if it's more than 3 characters show search results
-    if (searchTerm.length >= 3) {
-      setSearchResultsVisibility(true);
-    } else if (searchTerm.length === 0) {
-      setSearchResultsVisibility(false);
-      setShowMovieMoreInfo(false);
-    }
-    if (nominatedMoviesList.length > 0) {
-      setNominationsVisibility(true);
-    }
-  }, [searchTerm, nominatedMoviesList]);
-
-  const nominateMovie = (movie) => {
-    if (nominatedMoviesList.length < maxNominations - 1) {
-      movie.disableNominate = true;
-      const newNominationsList = [...nominatedMoviesList, movie];
-      setNominatedMoviesList(newNominationsList);
-      saveToLocalStorage(newNominationsList);
-    } else if (nominatedMoviesList.length === maxNominations - 1) {
-      movie.disableNominate = true;
-      const newNominationsList = [...nominatedMoviesList, movie];
-      setNominatedMoviesList(newNominationsList);
-      saveToLocalStorage(newNominationsList);
-      setNomCompleted(true);
-    } else {
-      setNomCompleted(true);
-    }
-  };
-
-  const removeNomination = (movie) => {
-    movie.disableNominate = false;
-    const newNominationsList = nominatedMoviesList.filter(
-      (nominatedMoviesList) => nominatedMoviesList.imdbID !== movie.imdbID
-    );
-    setNomCompleted(false);
-    setNominatedMoviesList(newNominationsList);
-    saveToLocalStorage(newNominationsList);
-  };
-
-  const saveToLocalStorage = (items) => {
-    localStorage.setItem("omdb-app-nominations", JSON.stringify(items));
-  };
-
-  const classes = useStyles();
-
-  // dark mode theming
-
-  const theme = createMuiTheme({
-    palette: {
-      type: "dark",
-    },
-  });
 
   return (
- 
-    <ThemeProvider theme={theme}>
-         <Helmet>
-          <title>OMDb Movies</title>
-        </Helmet>
-      <Grid container direction="column">
-        <Grid item xs={12}>
-          <SearchAppBar setSearchTerm={setSearchTerm} />
-        </Grid>
-        <Grid item xs={12} container direction="row">
-          <Grid item container direction="column" xs={5}>
-            <Grid item>
-              {searchResultsVisible ? (
-                <SearchResults
-                  searchTerm={searchTerm}
-                  currentPage={currentPage}
-                  setMaxPages={setMaxPages}
-                  onNominateClicked={nominateMovie}
-                  onSearchEntryClicked={onSearchEntryClicked}
-                  nominatedMoviesList={nominatedMoviesList}
-                ></SearchResults>
-              ) : null}
-            </Grid>
-          </Grid>
-          <Grid
-            item
-            container
-            direction="row"
-            xs={showMovieMoreInfo ? 3 : 6}
-            justify="center"
-          >
-            <Grid item xs={12}>
-              {nominatedMoviesList.length ? (
-                <Typography className={classes.nominationsHeader} variant="h4">
-                  Nominations
-                </Typography>
-              ) : null}
-              <NominatedMovies
-                movies={nominatedMoviesList}
-                onRemoveNominationClicked={removeNomination}
-              />
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <MovieMoreInfo visible={showMovieMoreInfo} movie={movie4MoreInfo} />
-          </Grid>
-        </Grid>
-      </Grid>
-      <PageControl
-        maxPages={maxPages}
-        minPages={1}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      ></PageControl>
-      <AlertDialog
-        hideButton={nominationsVisible}
-        open={nominationsCompleted}
-        nominations={nominatedMoviesList}
-      />
-    </ThemeProvider>
+    <Page
+      title="Polaris"
+    >
+
+    </Page>
   );
 }
 
-export default App;
